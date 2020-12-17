883fb2faecb0c0d69e0d7a9e2be28acb1c6de7dc
SREEHARI <sreehari2610@gmail.com>
2020-12-17 20:31:51 +0530

    banner initial

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 import'./Home.css';
 function Home() {
     return (
         <div>
-            Home
+           <Banner/>
         </div>
     )
 }
